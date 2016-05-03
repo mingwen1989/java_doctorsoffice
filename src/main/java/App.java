01f6fc2c13a064bb8b14c2c07d0efbead287e700
Garrett Biernat and Ming Wen <mwen82@gmail.com>
2016-05-03 13:15:07 -0700

    Stuck on COUNT SQL function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,15 +13,71 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/home.vtl");
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/results", (request, response) -> {
+    get("/doctors/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/results.vtl");
+      model.put("template", "templates/doctor-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    get("/patients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("doctors", Doctor.all());
+      model.put("template", "templates/patient-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    post("/doctors", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String firstName = request.queryParams("firstName");
+      String lastName = request.queryParams("lastName");
+      String specialty = request.queryParams("specialty");
+
+      Doctor newDoctor = new Doctor(firstName, lastName, specialty);
+      newDoctor.save();
+
+      model.put("template", "templates/doctor-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/doctors", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("doctors", Doctor.all());
+      model.put("template", "templates/doctors.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/patients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String firstName = request.queryParams("firstName");
+      String lastName = request.queryParams("lastName");
+      String birthdate = request.queryParams("birthday");
+      int doctorId = Integer.parseInt(request.queryParams("doctor"));
+
+      Patient newPatient = new Patient(firstName, lastName, birthdate, doctorId);
+      newPatient.save();
+
+      model.put("template", "templates/patient-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/patients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("patients", Patient.all());
+      model.put("template", "templates/patients.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/doctors/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Doctor doctor = Doctor.find(Integer.parseInt(request.params(":id")));
+      model.put("doctor", doctor);
+      model.put("template", "templates/doctor.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
---
+++ src/main/java/Doctor.java
@@ -0,0 +1,77 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Doctor {
+  private int id;
+  private String first_name;
+  private String last_name;
+  private String specialty;
+
+  public Doctor(String first_name, String last_name, String specialty) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.specialty = specialty;
+  }
+
+  public String getFirstName() {
+    return first_name;
+  }
+
+  public String getLastName() {
+    return last_name;
+  }
+
+  public String getSpecialty() {
+    return specialty;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Doctor> all() {
+    String sql = "SELECT * FROM doctors ORDER BY last_name ASC";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Doctor.class);
+    }
+  }
+  public List<T> countPatients() {
+    String sql = "SELECT COUNT(doctorid) FROM patients WHERE doctorid =:id";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql)
+      .addParameter("id", this.id)
+      .executeAndFetch(T);
+    }
+  }
+
+  public List<Patient> getPatients() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM patients where doctorid=:id";
+      return con.createQuery(sql)
+      .addParameter("id", this.id)
+      .executeAndFetch(Patient.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO doctors(first_name, last_name, specialty) VALUES (:first_name, :last_name, :specialty)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("first_name", this.first_name)
+      .addParameter("last_name", this.last_name)
+      .addParameter("specialty", this.specialty)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static Doctor find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM doctors where id=:id";
+      Doctor doctor = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Doctor.class);
+      return doctor;
+    }
+  }
+}
